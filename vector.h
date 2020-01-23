ce1a773aa9f8523802bf54a6e4083595865eb230
jordychetty <jxrrdy@gmail.com>
2020-01-23 14:42:24 +0200

    Added some docs

--- vector.h
+++ vector.h
@@ -1,15 +1,18 @@
-
-
 #pragma once
 #include <vector>
 #include <cmath>
+
+/**
+ * Basic vector operations
+ */
 
 namespace spivak
 {
     class vector 
     {
         private:
-            std::vector<float> c;
+            std::vector<float> c; // vector components
+            
             vector(std::vector<float> cc) 
             {
                 c = cc;
@@ -17,6 +20,7 @@ namespace spivak
 
             /**
              * vector addition
+             * @param w vector
              */
             spivak::vector add(spivak::vector w)
             {
@@ -31,10 +35,11 @@ namespace spivak
 
             /**
              * vector subtraction
+             * @param w vector
              */
             spivak::vector sub(spivak::vector w)
             {
-                                std::vector<float> temp;
+                std::vector<float> temp;
                 assert(dim() == w.dim());
                 for (int i = 0; i < c.size(); i++)
                 {
@@ -43,6 +48,10 @@ namespace spivak
                 return spivak::vector(temp);   
             }
 
+            /**
+             * scalar multiplication
+             * @param a scalar value
+             */
             spivak::vector mult(float a)
             {
                 std::vector <float> t;
@@ -53,6 +62,10 @@ namespace spivak
                 return spivak::vector(t);
             }
 
+            /**
+             * vector division (shorthand for multiplication by inverse)
+             * @param a divisor
+             */
             spivak::vector div(float a)
             {
                 std::vector <float> t;
@@ -63,6 +76,9 @@ namespace spivak
                 return spivak::vector(t);   
             }
 
+            /**
+             * calculates and returns magnitude
+             */
             float mag()
             {
                 float t = 0.0f;
@@ -73,6 +89,10 @@ namespace spivak
                 return std::sqrt(t);
             }
 
+            /**
+             * dot product of this vector with another vector w
+             * @param w vector
+             */
             float dot (spivak::vector w)
             {
                 assert(dim() == w.dim());
@@ -84,28 +104,47 @@ namespace spivak
                 return t;
             }
 
+            /**
+             * dot product, trigonometric formula
+             * @param w vector
+             * @param theta cosine angle
+             */
             float dot(spivak::vector w, float theta)
             {
                 return mag() * w.mag() * std::cos(theta);
             }
 
+            /**
+             * cross product
+             * @param w vector
+             */
             spivak::vector cross(spivak::vector w)
             {
                 // stub
             }
 
+            /**
+             * cross product magnitude
+             * @param w vector
+             * @param theta sine angle 
+             */
             float cross_mag(spivak::vector w, float theta)
             {
                 return mag() * w.mag() * std::sin(theta);
             }
 
-
-
+            /**
+             * projection of vector onto another vector, w
+             * @param w vector
+             */
             spivak::vector projection(spivak::vector w)
             {
                 return mult(dot(w) / std::pow(mag(), 2.0f));
             }
 
+            /**
+             * unit vector of this vector
+             */
             spivak::vector unit()
             {
                 std::vector<float> temp;
@@ -116,9 +155,15 @@ namespace spivak
                 return spivak::vector(temp);
             }
 
-
+            /**
+             * returns the i-th component of this vector
+             * @param i i-th component
+             */
             float get(int i) { return c[i]; }
-            int dim() { return c.size() + 1; }
+
+            /**
+             * returns number of dimensions of this vector
+             */
             int dim() { return c.size() + 1; }
     };
 }
