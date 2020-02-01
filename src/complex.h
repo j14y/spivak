6e9da1853392288af2858d10185b0eefce864bb7
jordychetty <jxrrdy@gmail.com>
2020-02-01 19:03:42 +0200

    Added complex operations

--- src/complex.h
+++ src/complex.h
@@ -20,8 +20,25 @@ namespace spivak
             float a, b;
             float i;
             spivak::Operation op;
+            
+            /**
+             * exponential function, defined as the power series sum^{n}_{0} z^n/n!.
+             * @param n upper summation limit
+             * @param c current index
+             * @param s sum
+             * @param r given radius of convergence
+             * @return value of exponential function within given radius of convergence
+             */
+            float exp(int n, int c, float s, float r)
+            {
+                if ((s = std::pow()/))
+                {
+
+                } else return exp(n, c++, s, r);
+            }
 
         public:
+            complex() {}
 
             complex (float a, float b) 
             {
@@ -75,21 +92,50 @@ namespace spivak
                 return spivak::complex(re()/denom, im()/denom, Operation::SUB);
             }
 
+            /**
+             * complex square root
+             */
             std::pair<spivak::complex, spivak::complex> sqr_root()
             {
-                std::make_pair();
+                float gamma = std::sqrt((re() + std::sqrt(std::pow(re(), 2.0f) + std::pow(im(), 2.0f)))/2.0f);
+                float delta = sgn(this->b) * std::sqrt(( -re() + std::sqrt(std::pow(re(), 2.0f) + std::pow(im(), 2.0f)))/2.0f);
+                std::pair<spivak::complex, spivak::complex> sol = std::make_pair(spivak::complex(gamma, delta, Operation::ADD), spivak::complex(gamma, delta, Operation::SUB));
             }
 
+            /**
+             * complex modulus 
+             * sqrt(a^2 + b^2)
+             */
+            float modulus ()
+            {
+                return std::sqrt(std::pow(re(), 2.0f) + std::pow(im(), 2.0f));
+            }
+            
+            /**
+             * exponential function. Calls {@link exp(int, int, float, float)} internally.
+             * @param n upper summation limit
+             * @param r radius of convergence
+             */
+            float exp(int n, float r)
+            {
+                exp(n, 0.0f, 0.0f, r);
+            }
 
             // relations and operations
             // src: https://en.wikipedia.org/wiki/Complex_number#Definition
 
-            // Two complex numbers are equal if and only if both their real and imaginary parts are equal
+            /**
+             * complex equality
+             * @param n complex number
+             */
             bool equal(spivak::complex n)
             {
                 return (re() == n.re()) && (im() == n.im());
             }
 
+            /**
+             * finds and returns the complex conjugate of the complex object
+             */
             spivak::complex conjugate()
             {
                 return (this->op == Operation::ADD) ? 
@@ -97,12 +143,35 @@ namespace spivak
                     spivak::complex(re(), im(), Operation::ADD);
             }
 
-            // mutate 
-            void set_re(float a) { this->a = a; }
-            void set_im(float b) { this->b = b; }
+            /*********************
+             * utility functions *
+             *********************/
 
-            // accessors
-            float re() { return a; }
-            float im() { return b; }
+            /**
+             * factorial function
+             * @param n factorial
+             * @param factorial value of n
+             */
+            inline int factorial(int n)
+            {
+                return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
+            }
+
+            /**
+             * sign function/signum
+             * @param a nonnegative real number
+             * @return signum of a
+             */
+            inline float sgn(float a) 
+            {
+                if (a < 0)          return -1.0f;
+                else if (a == 0.0f) return 0.0f;
+                else                return 1.0f;
+            }
+
+            inline void set_re(float a) { this->a = a; }
+            inline void set_im(float b) { this->b = b; }
+            inline float re() { return a; }
+            inline float im() { return b; }
     };
 }
